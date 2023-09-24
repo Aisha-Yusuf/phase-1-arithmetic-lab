41281f8bfbbb8af8e5f4d10f51866e688532e7b2
Aisha-Yusuf <ayshaofficiel@gmail.com>
2023-09-24 16:41:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random() * 100); 
+
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+
+const numArray = [3, 9, 20, 8, 15];
+const max = Math.max(...numArray);
+
+console.log(`multiply: ${multiply}`);
+console.log(`random: ${random}`);
+console.log(`mod: ${mod}`);
+console.log(`max: ${max}`);
+
+
